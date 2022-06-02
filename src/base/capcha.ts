f739e5ad1b1590b8ec8bd6e142715af71c2cd9a2
curtis <curtis@fillygaming888.com>
2022-06-03 05:34:05 +0800

    Update

--- src/base/capcha.ts
+++ src/base/capcha.ts
@@ -1,10 +1,11 @@
+import { assert } from "common_js_builtin/dist/utils/assert";
+import { is } from "common_js_builtin/dist/utils/typeInferernce";
+import CryptoJS from "crypto-js";
 
 type none = null | undefined;
 type CaptchaCallback = (response: "success" | "error", captcha: Element, numberOfTries: number) => void;
 
-
-export
-type CaptchaOptions = {
+export type CaptchaOptions = {
   el: string;
   canvasClass?: string | none;
   numbersOfTries: number;
@@ -19,21 +20,25 @@ type CaptchaOptions = {
     width: number ;
     height: number ;
     font?: string | none;
+    fontSize?: string | none;
     fillStyle?: string | none;
-    textAlign?: string | none;
-    textBaseline?: string | none;
+    textAlign?: CanvasTextAlign | none;
+    textBaseline?: CanvasTextBaseline | none;
   };
 }
 
 function _setCanvas($el: Element, options: CaptchaOptions){
-  const child: HTMLCanvasElement = new HTMLCanvasElement();
+  // const lastChild = $el.children[$el.children.length -1];
+  // if (lastChild.constructor.name == "HTMLCanvasElement")
+  //   $el.removeChild(lastChild);
+  const child: HTMLElement = document.createElement("section");
   child.innerHTML = `<canvas 
-        class="${options.canvasClass}"
-        width="${options.canvasStyle.width}" 
-        height="${options.canvasStyle.height}">
-      </canvas>`;
-  $el.innerHTML = '';
-  $el.appendChild(child);
+      class="${options.canvasClass}"
+      width="${options.canvasStyle.width}" 
+      height="${options.canvasStyle.height}">
+    </canvas>`;
+  $el.innerHTML = "";
+  $el.appendChild(child.children[0]);
 }
 
 const defaultOption: CaptchaOptions = {
@@ -56,6 +61,26 @@ const defaultOption: CaptchaOptions = {
   }
 };
 
+const SOLID = "POSITIVE_NEGATIVE";
+
+export
+const enc = (text:string) => {
+  return CryptoJS.AES.encrypt(text, SOLID).toString();
+};
+
+export
+const dec = (ciphertext:string) => {
+  try {
+    const bytes = CryptoJS.AES.decrypt(ciphertext, SOLID);
+    return bytes.toString(CryptoJS.enc.Utf8);
+  } catch (e) {
+    throw e;
+  }
+};
+
+function transformFont(font: string, fontSize: string){
+  return `${fontSize} ${font}`;
+}
 export
 class Captcha{
   $el?: Element | none;
@@ -64,9 +89,12 @@ class Captcha{
   currentRetries: number = 0;
 
   constructor(public options: CaptchaOptions = defaultOption) {
-    this.options = Object.assign(options, defaultOption);
+    this.options = Object.assign(defaultOption, options);
+    this.options.canvasStyle.font = transformFont(this.options.canvasStyle.font!, this.options.canvasStyle.fontSize!);
+
     document.querySelector(this.options.el!);
     this.$el = document.querySelector(this.options.el!);
+    assert(is.not.empty(this.$el), `unexpect null dom, selector: ${this.options.el}`);
     this.options.beforeGenerateCaptcha(this);
     this.setCaptcha(false);
   }
@@ -75,8 +103,19 @@ class Captcha{
     this.$captchaTextContext!.clearRect(0, 0, this.options.canvasStyle.width, this.options.canvasStyle.height);
   }
 
-  fillText(text: string, {x, y} = {x:0, y:0}){
+  fillText(text: string, x?:number, y?:number){
+    x ??= this.$captchaEl!.clientWidth! / 2 - 20;
+    y ??= this.$captchaEl!.clientHeight! / 2;
     this.$captchaTextContext!.fillText(text, x, y);
+    this.$captchaTextContext!.font = ""
+    this.$captchaTextContext!.textAlign = this.options.canvasStyle.textAlign!;
+    this.$captchaTextContext!.textBaseline = this.options.canvasStyle.textBaseline!
+  }
+
+  resize(width: number, height: number){
+    this.options.canvasStyle.width = width
+    this.options.canvasStyle.height = height
+    this.setCaptcha(true);
   }
 
   setCaptcha(shouldReset: boolean) {
@@ -85,7 +124,7 @@ class Captcha{
     if (!shouldReset) {
       _setCanvas($el, options);
 
-      this.$captchaEl = document.querySelector(`.${options.canvasClass}`) as HTMLCanvasElement;
+      this.$captchaEl = $el.querySelector(`.${options.canvasClass}`) as HTMLCanvasElement;
       this.$captchaTextContext = this.$captchaEl.getContext('2d');
       this.$captchaTextContext = Object.assign(this.$captchaTextContext, options.canvasStyle);
     }
@@ -94,7 +133,10 @@ class Captcha{
 
   validate() {
     this.currentRetries++;
+    console.log(1);
     if (this.options.numbersOfTries < this.currentRetries){
+
+      console.log(2);
       if(this.options.onMaxRetries(this)){
         this.currentRetries = 0;
         this.reset();
@@ -102,7 +144,8 @@ class Captcha{
       }else{
       }
     }
-    if (!this.options.onValidate(this)) {
+    console.log(3);
+    if (this.options.onValidate(this)) {
       this.options.onValidateSuccess(this);
     } else {
       this.options.onValidateError(this);
@@ -114,5 +157,4 @@ class Captcha{
     this.setCaptcha(true);
   }
 }
-
 
